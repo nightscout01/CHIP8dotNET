284165c7008fe201f8dccc03b5cb0ec6c2b416db
Maurice Montag <rmontag@uw.edu>
2019-12-26 00:54:44 -0800

    Add project files.

---
+++ CHIP8EMU/GlobalMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace CHIP8EMU
+{
+    class GlobalMain  // split this class up when needed
+    {
+        private const string ROM_PATH = @"C:\Users\night\Downloads\DivisionTest.ch8";  // path to rom image to load
+        static void Main(string[] args)
+        {
+            CHIP8 chip8 = new CHIP8();  // temp for now
+            byte[] romToLoad = File.ReadAllBytes(ROM_PATH);
+            chip8.LoadProgram(romToLoad);
+            chip8.BeginEmulation();
+            Console.WriteLine("press enter to exit at any time");
+            Console.ReadLine();  // stop when user presses a key
+        }
+    }
+}
